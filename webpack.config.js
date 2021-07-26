e3d26f8b41435762018d4fb169af3dd13b5b9cfc
olawale-o <omoogunolawale@gmail.com>
2021-07-26 06:34:22 +0100

    Add content to index.html file

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
   entry: {
     index: './src/index.js',
   },
-  plugin: [
+  plugins: [
     new HTMLWebpackPlugin({
       filename: 'index.html',
       template: './src/index.html',
